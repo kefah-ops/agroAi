f339be024dda5bef83b7fa843b2a11e346aaa99b
Kefah-ops <ndirangukefah@gmail.com>
2025-10-21 22:07:16 +0300

    initial commit

--- run.py
+++ run.py
@@ -4,8 +4,10 @@ import os
 # Ensure Python can find your app package
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 
-from app import create_app, db  
+from app import create_app, db
+
 app = create_app()
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # Railway provides PORT dynamically
+    app.run(host="0.0.0.0", port=port)
