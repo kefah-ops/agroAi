1148a26c77ac6cad9e28d45b8feb320186071c24
Kefah-ops <ndirangukefah@gmail.com>
2025-10-22 14:31:33 +0300

    Move app instance outside main block for Gunicorn

--- run.py
+++ run.py
@@ -6,6 +6,7 @@ sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 
 from app import create_app, db
 
+# 👇 This must be outside so Gunicorn can detect it
 app = create_app()
 
 if __name__ == "__main__":
